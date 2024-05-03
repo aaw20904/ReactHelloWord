0224272d07780526dfcde3241380f9d8173cd1e6
Andrii Androsovych <kozakizdona@gmail.com>
2024-05-03 15:35:41 +0300

    jkghfjhg

--- public/js/index.jsx
+++ public/js/index.jsx
@@ -1,22 +1,44 @@
-function MyButton() {
-  return (
-    <button className="btn btn-success">
-      I'm a button
-    </button>
-  );
-}
-
-const App = () => {
    
+    let usr="Misha";
+    
+      const user = {
+        name: 'Hedy Lamarr',
+        imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+        imageSize: 90,
+      };
 
-    return (
-        <div className="p-2">
-            <h1>Hello, React!</h1>
-            <p>This is a simple React application.</p>
-           <MyButton />
-        </div>
-    );
-}
+    const App = () => {
 
-// Render the component
-ReactDOM.render(<App />, document.getElementById('root'));
+            function MyButton() {
+            return (
+              <button className="btn btn-success">
+                I'm a button
+              </button>
+            );
+          }
+
+      usr=Date.now().toString();
+
+        return (
+          
+            <div className="p-2">
+                <h1>Hello, React!</h1>
+                <p>This is a simple React application.{usr}</p>
+              <MyButton />
+               <h1>{user.name}</h1>
+              <img
+                className="avatar"
+                src={user.imageUrl}
+                alt={'Photo of ' + user.name}
+                style={{
+                  width: user.imageSize,
+                  height: user.imageSize
+                }}
+              />
+            </div>
+        );
+    }
+
+    // Render the component
+    ReactDOM.render(<App />, document.getElementById('root'));
+ 
